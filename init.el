008eefed592ec31d8c89ac25219d9930e1eba51b
Vitalii Homonov <inbox@homonov.me>
2025-03-20 12:30:00 +0200

    experiment with using aider

--- init.el
+++ init.el
@@ -165,7 +165,8 @@
                               ;; "\\*cider-"
                               "\\*kaocha-output\\*"
                               shell-mode
-                              eshell-mode))
+                              eshell-mode
+                              comint-mode))
   :init
   (popper-mode)
   (popper-echo-mode)
@@ -501,3 +502,16 @@ Handy for quick init.el access."
               ("M-l" . copilot-accept-completion-by-line))
   :config
   (advice-add 'indent-for-tab-command :around #'rk/copilot-complete-if-active))
+
+
+(use-package aidermacs
+  :bind (("C-c a" . aidermacs-transient-menu))
+  ;; :config
+  ; Set API_KEY in .bashrc, that will automatically picked up by aider or in elisp
+  ;; (setenv "ANTHROPIC_API_KEY" "sk-...")
+  ; defun my-get-openrouter-api-key yourself elsewhere for security reasons
+  ;; (setenv "OPENROUTER_API_KEY" (my-get-openrouter-api-key))
+  :custom
+  ; See the Configuration section below
+  (aidermacs-use-architect-mode t)
+  (aidermacs-default-model "sonnet"))
